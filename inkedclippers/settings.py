c46547235a50b7047aab4a628752a45060b5d76e
Dylan <Dylanmcmahon187@gmail.com>
2025-03-29 03:16:43 +0000

    Debug off

--- inkedclippers/settings.py
+++ inkedclippers/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-0netrunner-inkedclipper-0aayr0u87zt.ws-eu118.gitpod.io', "inked-clippers-6a7245ad63c3.herokuapp.com"]
 
